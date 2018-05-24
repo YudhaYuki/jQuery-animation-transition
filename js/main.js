8ca0839ade2fb637c797e572fb0a14d8018b57e6
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-24 22:40:29 +0200

    FADE IN AND FADE OUT

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
     });
     */
 
-
     // SHOW
     /*
     $('.trigger').click(function() {
@@ -19,13 +18,59 @@ $(document).ready(function() {
     });
     */
 
-
     // TOGGLE
+    /*
     $('.trigger').click(function() {
         $('.element').toggle(2000, function() {
             console.log('transition done !')
         });
     });
+    */
+
+
+
+    // -------- FADE IN AND FADE OUT ----------------------------------
+
+    /*
+
+    $('.fadeIn').click(function() {
+        $('.element').fadeIn(2000, function() {
+            console.log(`transition done`);
+        });
+    });
+
+    $('.fadeOut').click(function() {
+        $('.element').fadeOut(2000, function() {
+            console.log(`transition done`);
+        });
+    });
+    */
+
+
+    /*
+    $('.fadeIn').click(function() {
+        $('.element').fadeTo(2000, 1, function() {
+            console.log(`transition done`);
+        });
+    });
+
+    $('.fadeOut').click(function() {
+        $('.element').fadeTo(2000, 0.3, function() {
+            console.log(`transition done`);
+        });
+    });
+    */
+
+
+    $('.fadeIn, .fadeOut').click(function() {
+        $('.element').fadeToggle(2000, function() {
+            console.log('Transition Complete !')
+        })
+    })
+
+
+
+
 
 });
 
