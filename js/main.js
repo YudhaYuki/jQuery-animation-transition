0e20fe918ddd56e4afed022bf333c0c4f580092d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-24 22:16:13 +0200

    SHOW and HIDE and TOGGLE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+    // HIDE
+    /*
+    $('.trigger').click(function() {
+        $('.element').hide(2000, function() {
+            console.log('transition done !')
+        });
+    });
+    */
+
+
+    // SHOW
+    /*
+    $('.trigger').click(function() {
+        $('.element').show(2000, function() {
+            console.log('transition done !')
+        });
+    });
+    */
+
+
+    // TOGGLE
+    $('.trigger').click(function() {
+        $('.element').toggle(2000, function() {
+            console.log('transition done !')
+        });
+    });
 
 });
 
